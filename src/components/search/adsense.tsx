df671a94403030316f3b61e1ae4f910b383ca1ba
freshlife001 <talent@mises.site>
2025-03-18 17:36:34 +0800

    fix: ads visibility

--- src/components/search/adsense.tsx
+++ src/components/search/adsense.tsx
@@ -10,18 +10,35 @@ export const insertAdSenseAd = (container: Element | null) => {
   const adElement = document.createElement("ins");
   adElement.className = "adsbygoogle";
   adElement.style.display = "block";
+  adElement.style.width = "100%";
+  
   adElement.setAttribute("data-ad-client", "ca-pub-5392438536247908"); // Replace with your AdSense Publisher ID
   adElement.setAttribute("data-ad-slot", "9043198183"); // Replace with your Ad slot ID
   adElement.setAttribute("data-ad-format", "horizontal");
-  adElement.setAttribute("data-full-width-responsive", "true");
+  adElement.setAttribute("data-full-width-responsive", "false");
 
   // Append the ad to the container
   container.prepend(adElement);
+
+  // Function to check and remove unfilled ads
+  const removeUnfilledAd = () => {
+      if (adElement.getAttribute("data-ad-status") === "unfilled") {
+        adElement.style.display = "none"; // Hide container if ad is unfilled
+      } else {
+        adElement.style.height = "auto";
+      }
+  };
+
+    // Observe changes in the ad element
+    const observer = new MutationObserver(removeUnfilledAd);
+    observer.observe(adElement, { attributes: true, attributeFilter: ["data-ad-status"] });
+
 
   // Push the ad to load
   try {
       (window as any).adsbygoogle = (window as any).adsbygoogle || [];
       (window as any).adsbygoogle.push({});
+      adElement.style.height = "0px";
   } catch (error) {
       console.error("AdSense error:", error);
   }

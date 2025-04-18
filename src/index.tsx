80a851906089cabaa21af7d133466548d8f5eb4e
freshlife001 <talent@mises.site>
2025-04-18 13:00:22 +0800

    fix: lint

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { analytics } from './utils/firebase';
 import App from './App';
 import '@/styles/global.css'
 import reportWebVitals from './reportWebVitals';
